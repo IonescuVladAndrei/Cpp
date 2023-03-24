dbe45b2936c0a2a260899f5353afbcc69621a9d7
IonescuVladAndrei <92846214+IonescuVladAndrei@users.noreply.github.com>
2023-03-24 16:29:41 +0200

    Update banana.cpp

--- Lab 07/banana.cpp
+++ Lab 07/banana.cpp
@@ -6,34 +6,34 @@ Banana::Banana() : pret_bucata(0)
 
 Banana::Banana(int pret_bucata)
 {
-  this->pret_bucata = pret_bucata;
+    this->pret_bucata = pret_bucata;
 }
 
 Banana::Banana(const Banana &obj)
 {
-  this->pret_bucata = obj.pret_bucata;
+    this->pret_bucata = obj.pret_bucata;
 }
 
 int Banana::ord()
 {
-  return 1; // primul element ca ordine
+    return 1; // primul element ca ordine
 }
 
 double Banana::getPretFinal()
 {
-  return (double)this->pret_bucata;
+    return (double)this->pret_bucata;
 }
 
 Banana &Banana::operator=(const Banana &obj)
 {
-  if (this != &obj)
-  {
-    this->pret_bucata = obj.pret_bucata;
-  }
-  return *this;
+    if (this != &obj)
+    {
+        this->pret_bucata = obj.pret_bucata;
+    }
+    return *this;
 }
 
 void Banana::afisare()
 {
-  std::cout << "\tBanana: pret_bucata: " << this->pret_bucata << std::endl;
+    std::cout << "\tBanana: pret_bucata: " << this->pret_bucata << std::endl;
 }
