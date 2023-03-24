1663873b08e3428d400f597f42808dfdca606ed0
IonescuVladAndrei <92846214+IonescuVladAndrei@users.noreply.github.com>
2023-03-24 16:33:02 +0200

    Update nuci.hpp

--- Lab 07/nuci.hpp
+++ Lab 07/nuci.hpp
@@ -5,18 +5,18 @@
 
 class Nuci : public Fructe // pretul final = cantitate/pret_100_grame
 {
-  int cantitate; // in grame
-  int pret_100_grame;
+    int cantitate; // in grame
+    int pret_100_grame;
 
 public:
-  Nuci();
-  Nuci(int, int);
-  Nuci(const Nuci &);
+    Nuci();
+    Nuci(int, int);
+    Nuci(const Nuci &);
 
-  Nuci &operator=(const Nuci &);
-  void afisare();
-  double getPretFinal();
-  int ord();
+    Nuci &operator=(const Nuci &);
+    void afisare();
+    double getPretFinal();
+    int ord();
 };
 
 #endif
